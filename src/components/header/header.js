53c4abdf4b969cc6728f9fde5f4de32a436e3101
Richard Harrington <rwharrington87@gmail.com>
2017-08-21 11:36:39 -0400

    remove extraneous import from header.js and reorder constants

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import classNames from 'classnames';
 import backArrow from '../../../assets/backArrow.png';
 import './header.less';
